10ca1be076cf912d9dbd1f9124025f4d0d35682a
TristanVarewijck <tristanconnect4@outlook.com>
2022-04-05 21:33:22 +0200

    dynamic page chaching

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
 // when static  files are changed update them under a new cache name
 const staticCacheName = "site-static-v1";
+const dynamicCache = "site-dynamic-v1";
 const assets = [
   "/",
   "/offline",
@@ -19,7 +20,6 @@ const assets = [
 self.addEventListener("install", (evt) => {
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
-      console.log("caching shell asssets");
       cache.addAll(assets);
     })
   );
@@ -45,7 +45,15 @@ self.addEventListener("fetch", (evt) => {
   evt.respondWith(
     // get assets from cache (otherwise make the fetch from the server)
     caches.match(evt.request).then((cacheRes) => {
-      return cacheRes || fetch(evt.request);
+      return (
+        cacheRes ||
+        fetch(evt.request).then((fetchRes) => {
+          return caches.open(dynamicCache).then((cache) => {
+            cache.put(evt.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
     })
   );
 });
